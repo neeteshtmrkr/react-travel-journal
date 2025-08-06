da608e0f1c703e21eea85e573a43100a66712292
neeteshtmrkr <tamrakarnitesh21@gmail.com>
2025-08-07 00:04:28 +0545

    Travel Journal - Passing Object as a Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,13 @@ import Header from "./components/header";
 import Entry from "./components/entry";
 import data from "./data";
 
-/**
- * Challenge:
- * - import the array of data from data.js
- * - map over the array to create an <Entry /> component
- *   for every item in the data array.
- * - display the array of Entry components in place of the current
- *   hard-coded <Entry /> instance.
- */
-
 export default function App() {
 
   const entryElements = data.map((entry) => {
     return(
       <Entry 
         key={entry.id}
-        /**
-         * First set of curly braces is for the Javascript and the second one represents the object.
-         */
-        img={entry.img}
-        title={entry.title}
-        country={entry.country}
-        googleMapsLink={entry.googleMapsLink}
-        dates={entry.dates}
-        text={entry.text}
+        entry = {entry}
       />
     )
   })
